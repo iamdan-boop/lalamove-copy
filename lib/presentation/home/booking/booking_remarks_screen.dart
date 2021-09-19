cc2c07279bcdb2955ab6361f2e894e484c3ff453
Kenji Velasco <thedeceptivesanta@gmail.com>
2021-09-20 03:32:56 +0800

    added booking remarks screen

--- lib/presentation/home/booking/booking_remarks_screen.dart
+++ lib/presentation/home/booking/booking_remarks_screen.dart
@@ -8,10 +8,14 @@ class BookingRemarks extends StatefulWidget {
 }
 
 class _BookingRemarksState extends State<BookingRemarks> {
+  final remarksController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         leading: IconButton(
           onPressed: () {},
           icon: const Icon(
@@ -19,6 +23,7 @@ class _BookingRemarksState extends State<BookingRemarks> {
             color: Colors.black,
           ),
         ),
+        elevation: 0,
         centerTitle: true,
         title: const Text(
           'Order remarks',
@@ -27,8 +32,52 @@ class _BookingRemarksState extends State<BookingRemarks> {
           ),
         ),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 10,
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              width: double.infinity,
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: SizedBox.expand(
+                child: TextFormField(
+                  maxLines: 6,
+                  controller: remarksController,
+                  decoration: InputDecoration(
+                    hintText:
+                        '''Add any important instructions about your delivery''',
+                    hintMaxLines: 2,
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    counterText:
+                        'Characters left: ${500 - remarksController.value.text.length}',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.deepOrange,
+              height: 50,
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Confirm',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
